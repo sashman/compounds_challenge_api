756641b7c3970a5e6f2e0870b908716e0f805900
sash stasyk <sashman90@gmail.com>
2020-12-02 23:44:20 +0000

    UPdating log level

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :compounds_challenge_api, CompoundsChallengeApiWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # ## SSL Support
 #
